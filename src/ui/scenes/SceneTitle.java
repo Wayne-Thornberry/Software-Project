e16baef5b4fa431fbad6e7815a4971354168ffd6
DanielMoran98 <33605732+DanielMoran98@users.noreply.github.com>
2018-04-21 18:41:45 +0100

    Added Options menu

--- src/ui/scenes/SceneTitle.java
+++ src/ui/scenes/SceneTitle.java
@@ -89,6 +89,86 @@ public class SceneTitle extends JPanel {
         this.setBackground(Color.RED);
 
         this.setOptionsVisible(false);
+
+
+        //////////////////////Options Menu///////////////////
+        JPanel optionsPanel = new JPanel();
+
+        optionsPanel.setLayout(new GridLayout(5,3));
+
+        JButton optionsButton1 = new JButton("Change Lives");
+        JButton optionsButton2 = new JButton("Change Seed");
+        JButton optionsButton3 = new JButton("Change Time");
+        JButton optionsButton4 = new JButton("Change Res");
+        JButton optionsButton5 = new JButton("Submit");
+        JButton optionsButton6 = new JButton("Submit");
+        JButton optionsLeaderboard = new JButton("Leaderboard");
+
+        JLabel optionsLabel1 = new JLabel("Enter lives: ");
+        JLabel optionsLabel2 = new JLabel("Enter seed: ");
+        JLabel optionsLabel3 = new JLabel("Enter time: ");
+        JLabel optionsLabel4 = new JLabel("Choose size: ");
+        JLabel optionsLabel5 = new JLabel("Label");
+        JLabel optionsLabel6 = new JLabel("Label");
+
+        JTextField optionsLives = new JTextField();
+        JTextField optionsSeed = new JTextField();
+        JTextField optionsTime = new JTextField();
+        String[] resolutions = {"1024x576", "1280x720" ,"1920x1080"};
+        JComboBox optionsResolution = new JComboBox(resolutions);
+
+
+
+        optionsPanel.add(optionsLabel1);
+        optionsPanel.add(optionsLives);
+        optionsPanel.add(optionsButton1);
+
+        optionsPanel.add(optionsLabel2);
+        optionsPanel.add(optionsSeed);
+        optionsPanel.add(optionsButton2);
+
+        optionsPanel.add(optionsLabel3);
+        optionsPanel.add(optionsTime);
+        optionsPanel.add(optionsButton3);
+
+        optionsPanel.add(optionsLabel4);
+        optionsPanel.add(optionsResolution);
+        optionsPanel.add(optionsButton4);
+
+        optionsPanel.add(new JLabel(""));
+        optionsPanel.add(new JLabel(""));
+        optionsPanel.add(optionsLeaderboard);
+
+        optionsButton1.setFont(new Font("Arial",1,20));
+        optionsLabel1.setFont(new Font("Arial",1,20));
+
+        optionsButton2.setFont(new Font("Arial",1,20));
+        optionsLabel2.setFont(new Font("Arial",1,20));
+        optionsButton3.setFont(new Font("Arial",1,20));
+        optionsLabel3.setFont(new Font("Arial",1,20));
+        optionsResolution.setFont(new Font("Arial",1,20));
+        optionsButton4.setFont(new Font("Arial",1,20));
+        optionsLabel4.setFont(new Font("Arial",1,20));
+        optionsButton4.setFont(new Font("Arial",1,20));
+        optionsLabel4.setFont(new Font("Arial",1,20));
+        optionsLeaderboard.setFont(new Font("Arial",1,20));
+
+        optionsButton1.setBackground(new Color(0x0078DE));
+        optionsButton1.setForeground(Color.WHITE);
+        optionsButton2.setBackground(new Color(0x0078DE));
+        optionsButton2.setForeground(Color.WHITE);
+        optionsButton3.setBackground(new Color(0x0078DE));
+        optionsButton3.setForeground(Color.WHITE);
+        optionsButton4.setBackground(new Color(0x0078DE));
+        optionsButton4.setForeground(Color.WHITE);
+        optionsLeaderboard.setBackground(new Color(0x0078DE));
+        optionsLeaderboard.setForeground(Color.WHITE);
+
+
+
+
+        pRightSpace.add(optionsPanel);
+        /////////////////////////////////////////////////////
         this.add(pLeftSpace);
         this.add(pRightSpace);
 
